69b976b178394757ff03974efc7ca2498fc74053
Sergey <fserega2014@yandex.ru>
2023-06-26 15:23:33 +0300

    feat: add up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import os from 'node:os'
 import { parseArg } from './cli/args.js'
+import { up } from './fs/up.js'
 
+let currentPath = os.homedir()
 const username = parseArg()
+process.stdout.write(`You are currently in ${currentPath}\n`)
 
 const echoInput = (chunk) => {
   const inputChunk = chunk.toString().trim()
@@ -11,12 +15,17 @@ const echoInput = (chunk) => {
     )
     process.exit()
   }
+  if (inputChunk === 'up') {
+    currentPath = up(currentPath) + '\n'
+  }
+  process.stdout.write(`You are currently in ${currentPath}`)
 }
 
 process.stdin.on('data', echoInput)
+
 process.on('SIGINT', () => {
   process.stdout.write(
-    `Thank you for using File Manager, ${username}, goodbye!`
+    `\nThank you for using File Manager, ${username}, goodbye!`
   )
   process.exit()
 })
