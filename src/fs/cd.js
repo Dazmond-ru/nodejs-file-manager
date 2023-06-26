77590972d58b68d4ad51f04e25cc2b42a0a877b3
Sergey <fserega2014@yandex.ru>
2023-06-26 19:18:26 +0300

    feat: add cp

--- src/fs/cd.js
+++ src/fs/cd.js
@@ -1,142 +1,118 @@
-import fs from "fs/promises";
-import path from 'node:path';
-import { chdir } from "process";
+import fs from 'fs/promises'
+import path from 'node:path'
+import { chdir } from 'process'
 
 const pathExists = (path) =>
   fs.stat(path).then(
     () => true,
     () => false
-  );
+  )
 
 export const cd = async (prevPath, chunkStringified, isDirectoryCheck) => {
-  let inputPath = chunkStringified.slice(3);
+  let inputPath = chunkStringified.slice(3)
 
-  if (inputPath.includes(' ') && !inputPath.includes('\"')) {
-    process.stdout.write(`Invalid input\n`);
+  if (inputPath.includes(' ') && !inputPath.includes('"')) {
+    process.stdout.write(`Invalid input\n`)
     return prevPath
-  } 
-
-  if (inputPath.includes('*') || inputPath.includes('?') || inputPath.includes('<') || inputPath.includes('>') || inputPath.includes('|')) {
-    process.stdout.write(`Invalid input\n`);
-    return prevPath
-  } 
-
-  let inputPathCheck = false;
-  let countDoubleMarks = 0;
-  let isDoubleMarksInDiskName = false;
-  let isInvalidPath = false;
-  let inputPathNormalize = path.normalize(inputPath);
-
-  const inputPathRes = inputPathNormalize.split(`${path.sep}`).map((item, index) => {
-    if (index === 0) {
-      if (item[0] === '\"') { 
-        countDoubleMarks++;
-        item = item.slice(1)
-      }
-      if (item[item.length - 1] === '\"') { 
-        countDoubleMarks++;
-        item = item.slice(0, -1)
-      }
-      if (/^[A-Za-z]:{1}/.test(item) && countDoubleMarks > 0) {
-        countDoubleMarks = 0;
-        isDoubleMarksInDiskName = true;
-      }
-      return item;
-    } else {
-      if (item[0] === '\"') { 
-        countDoubleMarks++;
-        item = item.slice(1)
-      }
-      if (item[item.length - 1] === '\"') { 
-        countDoubleMarks++;
-        item = item.slice(0, -1)
-      }
-      return item;
-    } 
-  }).join(path.sep);
-  if (countDoubleMarks % 2 === 1 || isDoubleMarksInDiskName || isInvalidPath) {
-    process.stdout.write(`Invalid input\n`);
-    return prevPath;
   }
-  inputPath = inputPathRes;
 
+  if (
+    inputPath.includes('*') ||
+    inputPath.includes('?') ||
+    inputPath.includes('<') ||
+    inputPath.includes('>') ||
+    inputPath.includes('|')
+  ) {
+    process.stdout.write(`Invalid input\n`)
+    return prevPath
+  }
+
+  let inputPathCheck = false
+  let countDoubleMarks = 0
+  let isDoubleMarksInDiskName = false
+  let isInvalidPath = false
+  let inputPathNormalize = path.normalize(inputPath)
+
+  const inputPathRes = inputPathNormalize
+    .split(`${path.sep}`)
+    .map((item, index) => {
+      if (index === 0) {
+        if (item[0] === '"') {
+          countDoubleMarks++
+          item = item.slice(1)
+        }
+        if (item[item.length - 1] === '"') {
+          countDoubleMarks++
+          item = item.slice(0, -1)
+        }
+        if (/^[A-Za-z]:{1}/.test(item) && countDoubleMarks > 0) {
+          countDoubleMarks = 0
+          isDoubleMarksInDiskName = true
+        }
+        return item
+      } else {
+        if (item[0] === '"') {
+          countDoubleMarks++
+          item = item.slice(1)
+        }
+        if (item[item.length - 1] === '"') {
+          countDoubleMarks++
+          item = item.slice(0, -1)
+        }
+        return item
+      }
+    })
+    .join(path.sep)
+  if (countDoubleMarks % 2 === 1 || isDoubleMarksInDiskName || isInvalidPath) {
+    process.stdout.write(`Invalid input\n`)
+    return prevPath
+  }
+  inputPath = inputPathRes
 
   if (path.normalize(inputPath) === path.sep) {
-    return prevPath.slice(0, 3);
+    return prevPath.slice(0, 3)
   }
 
- 
   if (isDirectoryCheck) {
     if (/^[A-Za-z]:{1}/.test(inputPath)) {
-      if (inputPath.length === 3 && inputPath[inputPath.length - 1] === '.') inputPath = inputPath.slice(0, -1) + `${path.sep}`
-        // if (await pathExists(path.join(inputPath))) {
-        //   return inputPath;
-        // } else {
-        //   process.stdout.write(`Operation failed\n`);
-        //   return prevPath;
-        // }
-        try {
-          console.log('chDirExitFirst', inputPath);
-          chdir(inputPath);
-          return inputPath;
-        } catch (err) {
-          process.stdout.write(`Operation failedFirst\n`);
-          return prevPath;
-        }
+      if (inputPath.length === 3 && inputPath[inputPath.length - 1] === '.')
+        inputPath = inputPath.slice(0, -1) + `${path.sep}`
+
+      try {
+        console.log('chDirExitFirst', inputPath)
+        chdir(inputPath)
+        return inputPath
+      } catch (err) {
+        process.stdout.write(`Operation failedFirst\n`)
+        return prevPath
+      }
     }
 
     try {
-      console.log('chDirExitSecond', path.join(prevPath, inputPath));
-      chdir(path.join(prevPath, inputPath));
-      return path.join(prevPath, inputPath);
+      console.log('chDirExitSecond', path.join(prevPath, inputPath))
+      chdir(path.join(prevPath, inputPath))
+      return path.join(prevPath, inputPath)
     } catch (err) {
-      process.stdout.write(`Operation failedSecond\n`);
-      return prevPath;
+      process.stdout.write(`Operation failedSecond\n`)
+      return prevPath
     }
   }
-
-  // if (isDirectoryCheck) {
-  //   try {
-
-  //     inputPathCheck = await fs.lstat(path.join(prevPath, inputPath));
-  //   } catch {
-  //     process.stdout.write(`Operation failed\n`);
-  //     return prevPath;
-  //   }
-
-  //   if (/^[A-Za-z]:{1}/.test(inputPath)) {
-  //     if (inputPath.length === 3 && inputPath[inputPath.length - 1] === '.') inputPath = inputPath.slice(0, -1) + `${path.sep}`
-  //       if (await pathExists(path.join(inputPath))) {
-  //         return inputPath;
-  //       } else {
-  //         process.stdout.write(`Operation failed\n`);
-  //         return prevPath;
-  //       }
-  //   }
-
-  //   if (await pathExists(path.join(prevPath, inputPath)) && inputPathCheck.isDirectory()) {
-  //     return path.join(prevPath, inputPath);
-  //   } else {
-  //     process.stdout.write(`Operation failed\n`);
-  //     return prevPath;
-  //   }
-  // }
 
   if (!isDirectoryCheck) {
     if (/^[A-Za-z]:{1}/.test(inputPath)) {
-      if (inputPath.length === 3 && inputPath[inputPath.length - 1] === '.') inputPath = inputPath.slice(0, -1) + `${path.sep}`
-        if (await pathExists(path.join(inputPath))) {
-          return inputPath;
-        } else {
-          return prevPath;
-        }
+      if (inputPath.length === 3 && inputPath[inputPath.length - 1] === '.')
+        inputPath = inputPath.slice(0, -1) + `${path.sep}`
+      if (await pathExists(path.join(inputPath))) {
+        return inputPath
+      } else {
+        return prevPath
+      }
     }
 
     if (await pathExists(path.join(prevPath, inputPath))) {
-      return path.join(prevPath, inputPath);
+      return path.join(prevPath, inputPath)
     } else {
-      return prevPath;
+      return prevPath
     }
   }
-
 }
--- src/index.js
+++ src/index.js
@@ -1,94 +1,64 @@
-import os from 'node:os';
-import { parseArg } from "./cli/args.js";
-import { up } from "./fs/up.js";
-import { cd } from './fs/cd.js';
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'process';
-import { list } from './fs/ls.js';
-import { cat } from './fs/cat.js';
-import { add } from './fs/add.js';
-import { rn } from './fs/rn.js';
+import os from 'node:os'
+import { parseArg } from './cli/args.js'
+import { up } from './fs/up.js'
+import { cd } from './fs/cd.js'
+import * as readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'process'
+import { list } from './fs/ls.js'
+import { cat } from './fs/cat.js'
+import { add } from './fs/add.js'
+import { rn } from './fs/rn.js'
 
-let currentPath = os.homedir();
-const username = parseArg(currentPath);
-const rl = readline.createInterface({ input, output });
+let currentPath = os.homedir()
+const username = parseArg(currentPath)
+const rl = readline.createInterface({ input, output })
 
-rl.on( 'line' , async (query) => {
+rl.on('line', async (query) => {
   if (query === '.exit') {
-    process.stdout.write(`\nThank you for using File Manager, ${username}, goodbye!`);
-    rl.close();
+    process.stdout.write(
+      `\nThank you for using File Manager, ${username}, goodbye!`
+    )
+    rl.close()
   }
 
   if (query === 'up') {
-    currentPath = up(currentPath);
-    process.stdout.write(`You are currently in ${currentPath}\n`);
+    currentPath = up(currentPath)
+    process.stdout.write(`You are currently in ${currentPath}\n`)
   }
 
   if (query.startsWith('cd ')) {
-    const isDirectoryCheck = true;
-    currentPath = await cd(currentPath, query, isDirectoryCheck);
-    process.stdout.write(`You are currently in ${currentPath}\n`);
+    const isDirectoryCheck = true
+    currentPath = await cd(currentPath, query, isDirectoryCheck)
+    process.stdout.write(`You are currently in ${currentPath}\n`)
   }
 
   if (query === 'ls') {
-    const folderContain = await list(currentPath);
-    console.table(folderContain);
-    process.stdout.write(`You are currently in ${currentPath}\n`);
+    const folderContain = await list(currentPath)
+    console.table(folderContain)
+    process.stdout.write(`You are currently in ${currentPath}\n`)
   }
 
   if (query.startsWith('cat ')) {
-    rl.pause();
-    await cat(currentPath, query);
-    process.stdout.write(`\nYou are currently in ${currentPath}\n`);
-    rl.resume();
-  }   
+    rl.pause()
+    await cat(currentPath, query)
+    process.stdout.write(`\nYou are currently in ${currentPath}\n`)
+    rl.resume()
+  }
 
   if (query.startsWith('add ')) {
-    await add(currentPath, query);
-    process.stdout.write(`\nYou are currently in ${currentPath}\n`);
-  }   
+    await add(currentPath, query)
+    process.stdout.write(`\nYou are currently in ${currentPath}\n`)
+  }
 
   if (query.startsWith('rn ')) {
-    await rn(currentPath, query);
-    process.stdout.write(`\nYou are currently in ${currentPath}\n`);
-  }  
-});
+    await rn(currentPath, query)
+    process.stdout.write(`\nYou are currently in ${currentPath}\n`)
+  }
+})
 
 rl.on('SIGINT', async () => {
-    process.stdout.write(`\nThank you for using File Manager, ${username}, goodbye!`); 
-    rl.close();
-});
-
-
-// rl.close();
-
-// const echoInput = async (chunk) => {
-//   const chunkStringified = chunk.toString().trim();
-
-//   if (chunkStringified === '.exit') {
-//     process.stdout.write(`Thank you for using File Manager, ${username}, goodbye!`);
-//     process.exit();
-//   }
-//   if (chunkStringified === 'up') {
-//     currentPath = up(currentPath);
-//     process.stdout.write('\n');
-//   }
-
-//   if (chunkStringified.startsWith('cd ')) {
-//     // console.log('11111\n')
-//     // currentPath = cd(currentPath, chunkStringified) + '\n';
-//     currentPath = await cd(currentPath, chunkStringified);
-//     process.stdout.write('\n');
-//   }
-//   process.stdout.write(`You are currently in ${currentPath}\n`);
-
-//   // process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
-// };
-
-// process.stdin.on('data', echoInput);
-
-// process.on('SIGINT', () => {
-//   process.stdout.write(`\nThank you for using File Manager, ${username}, goodbye!`); 
-//   process.exit();
-// });
-// // console.table([{name: 'Kek', name1: 'Kek1'}, {name: 'Kek2', name1: 'Kek3'}]);
+  process.stdout.write(
+    `\nThank you for using File Manager, ${username}, goodbye!`
+  )
+  rl.close()
+})
